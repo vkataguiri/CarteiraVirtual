a9ea03afb91d797549901b7c7f8bffa6faf8f9ec
vkataguiri <viniciuskataguiri@gmail.com>
2025-11-04 18:48:55 -0300

    chore: add packages and directories for file structuring

--- app/src/main/java/com/example/carteiravirtual/controller/ConverterRecursos.kt
+++ app/src/main/java/com/example/carteiravirtual/controller/ConverterRecursos.kt
@@ -1,16 +1,17 @@
-package com.example.carteiravirtual
+package com.example.carteiravirtual.controller
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.carteiravirtual.R
 
-class MainActivity : AppCompatActivity() {
+class ConverterRecursos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_converter_recursos)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
